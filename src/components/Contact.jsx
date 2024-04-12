16d88cbc9b8c6cdd9b22f6c29e51c92eeb3ec637
amitkrmodak <modakamit357@gmail.com>
2024-04-12 12:08:49 +0530

    contact form  validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
             ></textarea>
 
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Send
+              Save
             </button>
           </form>
         </div>
